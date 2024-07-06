c40272344f75e2e43294ef974f278b77f0f79d2a
urmi <urmitestbanerjeetest@gmail.com>
2024-07-06 19:05:24 -0400

    Fixed imports

--- src/test/java/nopCommerce_V1/landingPage.java
+++ src/test/java/nopCommerce_V1/landingPage.java
@@ -1,12 +1,10 @@
 package nopCommerce_V1;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
